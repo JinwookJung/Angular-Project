8fbf1e675eb938119929e96f0a25bff96315c7b6
Jinwook Jung <dddrdf@gmail.com>
2023-01-13 14:17:43 -0500

    added fetching data from MongoDB

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Subject } from "rxjs";
+import { map } from "rxjs/operators"
 
 import { Post } from "./post.model";
 
@@ -12,9 +13,21 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http
+      .get<{message: string, posts: any}>(
+      "http://localhost:3000/api/posts"
+      )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content : post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -26,7 +39,7 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http.
-      post<{message: string}>('http://localhost:3000/api/post', post)
+      post<{message: string}>("http://localhost:3000/api/post", post)
       .subscribe((responseData) => {
         console.log(responseData.message);
         this.posts.push(post);
