11dd67c76d59076bd4ff63b4344cb23106af8060
Jinwook Jung <dddrdf@gmail.com>
2023-01-12 22:03:12 -0500

    added nodeJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
@@ -26,6 +27,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    HttpClientModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,35 +1,34 @@
-import {Component, OnDestroy, OnInit} from "@angular/core";
-import { Subscription } from "rxjs";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from 'rxjs';
 
-
-import { Post } from '../post.model'
-import { PostsService } from "../post.service";
+import { Post } from "../post.model";
+import { PostsService } from "../posts.service";
 
 @Component({
-  selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  selector: "app-post-list",
+  templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.css"]
 })
-export class PostListComponent implements OnInit, OnDestroy{
-  // posts =[
-  //   {title: "Fisrt Post", content: "This is the first post's content"},
-  //   {title: "Second Post", content: "This is the second post's content"},
-  //   {title: "Third Post", content: "This is the third post's content"}
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   { title: "First Post", content: "This is the first post's content" },
+  //   { title: "Second Post", content: "This is the second post's content" },
+  //   { title: "Third Post", content: "This is the third post's content" }
   // ];
   posts: Post[] = [];
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
 
-  ngOnInit(): void {
-    this.posts = this.postsService.getPosts();
+  ngOnInit() {
+    this.postsService.getPosts();
     this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe((posts: Post[]) => {
         this.posts = posts;
       });
   }
 
-  ngOnDestroy(): void {
+  ngOnDestroy() {
     this.postsSub.unsubscribe();
   }
 }
