6f898940fff7f12e3b04b30c98a63918ab6449cb
Jinwook Jung <dddrdf@gmail.com>
2023-01-15 18:36:13 -0500

    added image controls and preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,8 @@
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { ReadKeyExpr } from "@angular/compiler";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Post } from "../post.model";
 
 import { PostsService } from "../posts.service";
 
@@ -8,17 +11,76 @@ import { PostsService } from "../posts.service";
   templateUrl: "./post-create.component.html",
   styleUrls: ["./post-create.component.css"]
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit{
   enteredTitle = "";
   enteredContent = "";
+  isLoading = false;
+  form: FormGroup;
+  imagePreview: string;
+  private mode = 'create';
+  post: Post;
+  private postId: string;
 
-  constructor(public postsService: PostsService) {}
 
-  onAddPost(form: NgForm) {
-    if (form.invalid) {
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+        content: new FormControl(null, {validators: [Validators.required]}),
+        image: new FormControl(null, {validators: [Validators.required]})
+    });
+
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')){
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.isLoading = true;
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.isLoading = false;
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content
+          };
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content});
+          });
+      }else{
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
-    form.resetForm();
+    this.isLoading = true;
+    if(this.mode === 'create'){
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
+    }else{
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content
+      );
+    }
+    this.form.reset();
   }
 }
