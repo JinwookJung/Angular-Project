ac7e51f8469db08ca4ad4295a4fe0dfd711a9622
Jinwook Jung <dddrdf@gmail.com>
2023-01-18 21:41:58 -0500

    added SPA Authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcrypt");//to hash password
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const user = require("../models/user");
 
 const router = express.Router();
 
@@ -25,6 +27,35 @@ router.post("/signup", (req, res, next) => {//new type of data needed
           });
         });
     });
+});
+
+router.post("/login", (req, res, next) => {
+  User.findOne({email: req.body.email })
+    .then(user => {
+      if(!user) {
+        return res.status(401).json({//Authentication denied
+          message: "Auth failed"
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if(!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        })
+      }
+      const token = jwt.sign(
+        { email: user.email, userId: user._id},
+         "secret_this_should_be_longer",
+        { expiresIn: "1h" }
+      );
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+      })
+    })
 });
 
 module.exports = router;
