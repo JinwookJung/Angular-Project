82204a45a86bd911ac713a1450e6179b5dfb15fc
Jinwook Jung <dddrdf@gmail.com>
2023-01-20 13:40:20 -0500

    connected the Logout button to the Authentication status

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -21,9 +21,15 @@ export class HeaderComponent implements OnInit, OnDestroy {
     });
   }
 
+  onLogout() {
+    this.authService.logout();
+  }
+
   ngOnDestroy() {
     this.authListenerSubs.unsubscribe();
   }
+
+
 
 
 }
