c5401f8e9c541f33ddcccb068004717bcf8c6d02
Jinwook Jung <dddrdf@gmail.com>
2023-01-20 11:55:48 -0500

    added the token to authenticate requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { Injectable, resolveForwardRef } from "@angular/core";
 import { response } from "express";
 
 import { AuthData } from "./auth-data.model";
@@ -8,8 +8,13 @@ import { AuthData } from "./auth-data.model";
 //it allows the service to be injected in Components or other service.
 @Injectable({ providedIn: "root"})
 export class AuthService {
+  private token: string;
+
   constructor(private http: HttpClient) {}
 
+  getToken(){
+    return this.token;
+  }
   createUser(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
     this.http.post("http://localhost:3000/api/user/signup", authData)
@@ -20,9 +25,10 @@ export class AuthService {
 
   login(email: string, password: string){
     const authData: AuthData = {email: email, password: password};
-    this.http.post("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
-        console.log(response);
+        const token = response.token;
+        this.token = token;
       })
   }
 }
