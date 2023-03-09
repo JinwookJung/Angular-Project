5fd9c66f361a6b70c9bc5706f0176e1a4d4b3999
Jinwook Jung <dddrdf@gmail.com>
2023-03-08 22:54:51 -0500

    Using the User ID on the Frontend

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -47,7 +47,12 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string, title: string, content: string, imagePath: string }>(
+    return this.http.get<{ _id: string,
+      title: string,
+      content: string,
+      imagePath: string,
+      creator: string
+    }>(
       "http://localhost:3000/api/posts/" + id
     );
   }
@@ -81,7 +86,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http
