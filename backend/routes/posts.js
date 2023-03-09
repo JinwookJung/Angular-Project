5d9657e8eebc29f7bb5f49f83c0f5d9665ea44a5
Jinwook Jung <dddrdf@gmail.com>
2023-03-08 22:29:17 -0500

    Protecting Resources with Authorization

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -57,6 +57,7 @@ router.post(
 
 router.put(
   "/:id",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
@@ -71,8 +72,13 @@ router.put(
       imagePath: imagePath
     });
     console.log(post);
-    Post.updateOne({ _id: req.params.id }, post).then(result => {
-      res.status(200).json({ message: "Update successful!" });
+    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
+      if(result.modifiedCount > 0){
+        res.status(200).json({ message: "Update successful!" });
+      }else{
+        res.status(200).json({ message: "Not authorized!" });
+      }
+
     });
   }
 );
@@ -112,10 +118,15 @@ router.get("/:id", (req, res, next) => {
 });
 
 router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-  });
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(
+    result => {
+      if(result.deletedCount > 0 ){
+        res.status(200).json({ message: "Deletion successful!" });
+      }else{
+        res.status(401).json({ message: "Not Authorized!" });
+      }
+  }
+  );
 });
 
 module.exports = router;
