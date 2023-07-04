4546e9375c8bb4525ad475c872a146df5b7eaab5
Jinwook Jung <dddrdf@gmail.com>
2023-07-04 13:32:54 -0400

    added Controllers

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 
-const Post = require("../models/post");
+
 const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
@@ -35,29 +35,7 @@ router.post(
   "",
   checkAuth,
   multer({ storage: storage }).single("image"),
-  (req, res, next) => {
-    const url = req.protocol + "://" + req.get("host");
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: url + "/images/" + req.file.filename,
-      creator: req.userData.userId
-    });
-    post.save().then(createdPost => {
-      res.status(201).json({
-        message: "Post added successfully",
-        post: {
-          ...createdPost,
-          id: createdPost._id
-        }
-      });
-    })
-    .catch(error => {
-      res.status(500).json({
-        message: "Creating a post failed!"
-      });
-    });
-  }
+
 );
 
 router.put(
