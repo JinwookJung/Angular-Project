e6a0d0a47bb3c06374485b80d0a7d481b155aad0
Jinwook Jung <dddrdf@gmail.com>
2023-07-11 16:50:53 -0400

    changed Production Budget and apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://elasticbeanstalk-us-east-2-407496447390.s3-website.us-east-2.amazonaws.com/api',
 };
